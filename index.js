b24906b4974157a28fe7ce2621afcbac8da2fae1
Gabi <gabee_88@yahoo.com>
2021-03-29 13:54:21 +0200

    feat: created user view

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ const path = require('path')
 */
 
 const app = express()
-const port = process.env.PORT || '3000'
+const port = process.env.PORT || '8000'
 
 /**
  *  App Configuration
@@ -18,13 +18,18 @@ const port = process.env.PORT || '3000'
 
 app.set('views', path.join(__dirname, 'views'))
 app.set('view engine', 'pug')
+app.use(express.static(path.join(__dirname, 'public')))
 
 /**
  * Routes Definitions
 */
 
 app.get('/', (req, res) => {
-  res.status(200).send('WHATABYTE: Food For Devs')
+  res.render('index', { title: 'Home' })
+})
+
+app.get('/user', (req, res) => {
+  res.render('user', { title: 'Profile', userProfile: { nickname: 'Gabi' } })
 })
 
 /**
